98606b5e0c3c8d7022205723f7c37d68a795ae45
Vincent Girès <mail@vincentgires.com>
2022-10-03 11:26:31 +0200

    Add separator setting

--- qtutils/widgets/combo.py
+++ qtutils/widgets/combo.py
@@ -2,13 +2,16 @@ from PySide2 import QtWidgets, QtCore, QtGui
 
 
 class CheckComboBox(QtWidgets.QComboBox):
-    """Editable ComboBox widget where each item can be added or removed with a
-    checkbox"""
+    """Editable ComboBox widget
 
-    def __init__(self, parent=None):
+    Each item can be added or removed with a checkbox.
+    """
+
+    def __init__(self, parent=None, separator=','):
         super().__init__(parent)
-        self._model = CheckComboItemModel(self)
-        self.setModel(self._model)
+        model = CheckComboItemModel(self)
+        model.set_separator(separator)
+        self.setModel(model)
         self.setEditable(True)
 
 
@@ -16,6 +19,9 @@ class CheckComboItemModel(QtGui.QStandardItemModel):
     def __init__(self, parent=None):
         super().__init__(parent)
         self._parent = parent
+
+    def set_separator(self, separator):
+        self.separator = separator
 
     def flags(self, index):
         return (QtCore.Qt.ItemIsEnabled |
@@ -26,7 +32,7 @@ class CheckComboItemModel(QtGui.QStandardItemModel):
         value = super(CheckComboItemModel, self).data(index, role)
         row = index.row()
         text = self._parent.lineEdit().text()
-        items = text.split(',')
+        items = text.split(self.separator)
         if role == QtCore.Qt.CheckStateRole:
             current_text = self._parent.itemText(row)
             if current_text in items:
@@ -38,8 +44,8 @@ class CheckComboItemModel(QtGui.QStandardItemModel):
     def setData(self, index, value, role):
         data = index.data()
         text = self._parent.lineEdit().text()
-        items = text.split(',') if text else []
+        items = text.split(self.separator) if text else []
         items.remove(data) if data in items else items.append(data)
-        result_string = ','.join(items)
+        result_string = self.separator.join(items)
         self._parent.setEditText(result_string)
         return True
