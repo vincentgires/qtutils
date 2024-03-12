8d484f6e5b862a5f51f0923f33148f506b48f728
Vincent Girès <mail@vincentgires.com>
2024-03-12 17:02:48 +0100

    Add icons support

--- qtutils/widgets/toolbutton.py
+++ qtutils/widgets/toolbutton.py
@@ -5,7 +5,7 @@ from PySide2 import QtCore, QtWidgets
 class PresetSelect(QtWidgets.QToolButton):
     selected = QtCore.Signal(str)
 
-    def __init__(self, parent, items):
+    def __init__(self, parent, items, icons=None):
         super().__init__(parent=parent)
         self.setPopupMode(QtWidgets.QToolButton.InstantPopup)
         actions = [QtWidgets.QAction(p, parent) for p in items]
@@ -14,3 +14,6 @@ class PresetSelect(QtWidgets.QToolButton):
         self.setMenu(menu)
         for action, name in zip(actions, items):
             action.triggered.connect(partial(self.selected.emit, name))
+        if icons is not None:
+            for action, icon in zip(actions, icons):
+                action.setIcon(icon)
