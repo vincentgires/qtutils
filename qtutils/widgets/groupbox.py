86785c09fdc6678fa1f04a6a67db9c5176d468b4
Vincent Girès <mail@vincentgires.com>
2024-09-03 12:03:05 +0200

    Add CollapsibleBox widget

---
+++ qtutils/widgets/groupbox.py
@@ -0,0 +1,19 @@
+from PySide2 import QtWidgets
+
+
+class CollapsibleBox(QtWidgets.QGroupBox):
+    def __init__(self, horizontal: bool = False):
+        super().__init__()
+        self.setCheckable(True)
+
+        # Layout
+        self.layout = (
+            QtWidgets.QHBoxLayout() if horizontal else QtWidgets.QVBoxLayout())
+        frame = QtWidgets.QFrame()
+        widget_layout = QtWidgets.QVBoxLayout()
+        widget_layout.addWidget(frame)
+        frame.setLayout(self.layout)
+        self.setLayout(widget_layout)
+
+        # Connection
+        self.toggled.connect(lambda x: frame.show() if x else frame.hide())
